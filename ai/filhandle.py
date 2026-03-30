c748f1261c6f35aea4c342927cf6205945a30827
anup <aniruddharai39@gmail.com>
2026-03-30 19:22:07 +0545

    dftg
    dfg

--- ai/filhandle.py
+++ ai/filhandle.py
@@ -4,6 +4,11 @@ user = input ("enter the number:")
 try :
     if int(user)>18:
         print("you are above 18")
+        inp = input("enter the file name:")
+        with open(inp, 'r') as f:
+            data = f.read()
+            print(data)
+
     else:        
         print("you are below 18")
 except ValueError:
